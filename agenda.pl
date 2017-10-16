f55b30b4c5c2d47625251301cc34286cd3a6cc5a
Deodato <F9600503@verescence.com>
2017-10-16 15:57:49 -0200

    Loop com as variaveis de decisão
    Signed-off-by: Deodato <F9600503@verescence.com>

--- agenda.pl
+++ agenda.pl
@@ -2,11 +2,25 @@
 
 	use Spreadsheet::Read qw(ReadData);	
 	my $agenda = ReadData ('Controlede_Contas_TI.xlsx');
+
+	($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+	$dia = $mday;
+	$contador = 0;
+	$venciTB = 3;
+	$nomeConta = 3; 	
+	#print 'Vivo Assinatura Mensal: ' . $agenda->[1]{C3};
 	
+	print "teste";
 	
-	print 'Vivo Assinatura Mensal: ' . $agenda->[1]{C3};
-	
-	#print "teste";
-	
+
+		while($contador <16) {
+		
+		print "----------------------\n";
+		print  $agenda->[1]{B.$nomeConta} . ' vencimento -' . $agenda->[1]{C.$venciTB};
+		
+		$venciTB++;
+		$nomeConta++;
+		$contador++;	
+		}
 	
 	
