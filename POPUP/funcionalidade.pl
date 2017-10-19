482cea8c40a630122fc257bd784c890429694d9b
fdeodato11 <fdeodato11@gmail.com>
2017-10-19 12:43:58 -0200

    popup modelo
    Signed-off-by: fdeodato11 <fdeodato11@gmail.com>

--- POPUP/funcionalidade.pl
+++ POPUP/funcionalidade.pl
@@ -1,31 +1,8 @@
 #!C:/Perl/bin
 
 
-my $toolbar = $mw->Frame->pack(qw/-fill x -expand 1/);
-my $file = $toolbar->Menubutton(
-    -text   => 'File',
-    -relief => 'raised',
-);
-$file->pack(qw/-side left/);;
-
-my $menu = $file->Menu(-tearoff => 0, -menuitems => [
-    [qw/command ~New/],
-    '',
-    [qw/command ~Open/],
-    '',
-    [qw/command ~Save/],
-    '',
-    [qw/command ~Close/],
-    '',
-    [qw/command ~Quit/, -command => \&exit],
-]);
-$file->configure(-menu => $menu);
-
-my $menu_index = 0;
-my $cursor = $toolbar->LabOptionmenu(
-    -label    => 'menu_index',
-    -labelPack => [qw/-side left/],
-    -variable => \$menu_index,
-    -options  => [(0, 2, 4, 6, 8)],
-);
-$cursor->pack(qw/-side right/);
+use Tk;
+my $mw = MainWindow->new;
+$mw->title("Hello World");
+$mw->Button(-text => "Done", -command => sub { exit })->pack;
+MainLoop;
