cd83f3a00fc32fff7a22e1f90f4800b2f231b6e6
fdeodato11 <fdeodato11@gmail.com>
2017-10-24 15:52:17 -0200

    Funcionalidade REGEX coletando Nome Conta
    Signed-off-by: fdeodato11 <fdeodato11@gmail.com>

--- TIME/data.pl
+++ TIME/data.pl
@@ -1,10 +1,11 @@
 #!C:/Perl/bin
 
 
-my $stringTeste = "B02229;DirectNet  (NeoVia);11;; GBR036 ;18/10/2017;;";
+my $stringTeste = "B02229;DirectNet_(NeoVia);11;; GBR036 ;18/10/2017;;";
 
 
-$stringTeste =~ /B0\d*/;
+$stringTeste =~ /B0\d*;(\w*\W+\w*\W+);/;
 
 
-print $&;
+
+print $1;
