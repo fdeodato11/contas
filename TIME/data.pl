fdb140cb5cd389dc8242dcf1b752ce6efbb97ff7
fdeodato11 <fdeodato11@gmail.com>
2017-10-24 16:05:08 -0200

    REGEX data de vencimento
    Signed-off-by: fdeodato11 <fdeodato11@gmail.com>

--- TIME/data.pl
+++ TIME/data.pl
@@ -1,11 +1,11 @@
 #!C:/Perl/bin
 
 
-my $stringTeste = "B02229;DirectNet_(NeoVia);11;; GBR036 ;18/10/2017;;";
+my $stringTeste = "B02229;DirectNet_(NeoVia);11;; GBR036 ;18;;";
 
 
-$stringTeste =~ /B0\d*;(\w*\W+\w*\W+);/;
+$stringTeste =~ /B0\d*;(\w*\W+\w*\W+);(\d*)/;
 
 
 
-print $1;
+print $2;
