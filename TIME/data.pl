2cabdfd6a5692d7ceeda7635644d7cf152f79495
fdeodato11 <fdeodato11@gmail.com>
2017-10-26 16:06:01 -0200

    primeiro if cuncional completo
    Signed-off-by: fdeodato11 <fdeodato11@gmail.com>

--- TIME/data.pl
+++ TIME/data.pl
@@ -1,17 +1,39 @@
 #!C:/Perl/bin
 
+($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+	my $dia = $mday;
+	
+	
+my $stringTeste = "B00922;VivoG;25;;GBR036;18;19;20;26";
 
-my $stringTeste = "B00922;VivoG;25;;GBR036;18;19;19;25";
 
-
-$stringTeste =~ /B0\d*;(\w*);(\d*);;(GBR\d*);(\d*);(\d*);(\d*);(\d*)/;
+$stringTeste =~ /B0\d*;(\w*);(\d*);;GBR\d*;(\d*);(\d*);(\d*);(\d*)/;
 
 
 
-print $1;
-print $2;
-print $3;
-print $4;
-print $5;
-print $6;
-print $7;
+#print $1;   # Nome da conta	
+#print $2;	# Data de vencimento
+#print $3;	# Data de recebimento da conta
+#print $4;   # Data de criação de requisição
+#print $5;   # Data de criação de pedido
+#print $6;   # Data da ultima aprovação
+
+
+
+#If de 
+	
+	
+
+
+
+
+
+
+	if ( $6 != null) {
+		print $6-$5. " dias para aprovacao";}
+		else {
+		print $dia-$5. " dias em aprovacao!!!";}
+		
+		
+		
+		
