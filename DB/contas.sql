706d0860edd2bc967fc32335b2ae513aa68bd7f8
Felipe <fdeodato11@gmail.com>
2018-01-17 16:09:25 -0200

    insert contas
    Signed-off-by: Felipe <fdeodato11@gmail.com>

--- DB/contas.sql
+++ DB/contas.sql
@@ -42,3 +42,56 @@
 			FOREIGN KEY (NOME_MES) REFERENCES mes(NOME),
 			FOREIGN KEY (NOME_CONTA) REFERENCES conta(NOME)
 		);
+		
+		
+		
+		
+		
+------------------------------------------------------------------------
+INSERTS
+------------------------------------------------------------------------
+
+INSERT INTO contas (ID,NOME)
+VALUES (1, "Vivo Movel");
+
+INSERT INTO contas (ID,NOME)
+VALUES (2, "Vivo 3G");
+
+INSERT INTO contas (ID,NOME)
+VALUES (3, "Vivo IP Dedicado");
+
+INSERT INTO contas (ID,NOME)
+VALUES (4, "Vivo Monitora");
+
+INSERT INTO contas (ID,NOME)
+VALUES (5, "Claro 3G");
+
+INSERT INTO contas (ID,NOME)
+VALUES (6, "Embratel Vipphone");
+
+INSERT INTO contas (ID,NOME)
+VALUES (7, "Embratel Link Internet");
+
+INSERT INTO contas (ID,NOME)
+VALUES (8, "Nitromidia");
+
+INSERT INTO contas (ID,NOME)
+VALUES (9, "Planus Taxa Fixa");
+
+INSERT INTO contas (ID,NOME)
+VALUES (10, "Planus Taxa Variavel");
+
+INSERT INTO contas (ID,NOME)
+VALUES (11, "Konics");
+
+INSERT INTO contas (ID,NOME)
+VALUES (12, "Engineering");
+
+INSERT INTO contas (ID,NOME)
+VALUES (13, "DirectNet");
+
+INSERT INTO contas (ID,NOME)
+VALUES (14, "NETVirtua");
+
+
+
