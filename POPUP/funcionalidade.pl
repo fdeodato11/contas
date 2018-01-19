57f7f342d04052b61b8b953cd292980333c3034c
Felipe <fdeodato11@gmail.com>
2018-01-19 15:04:50 -0200

    POP up ganhando forma
    Signed-off-by: Felipe <fdeodato11@gmail.com>

--- POPUP/funcionalidade.pl
+++ POPUP/funcionalidade.pl
@@ -3,6 +3,21 @@
 
 use Tk;
 my $mw = MainWindow->new;
+$mw->geometry("309x439");
 $mw->title("Auxiliar de Contas");
-$mw->Button(-text => "TESTE", -command => sub { exit })->pack;
+
+
+
+$mw->Button(-text => 'Receber conta', -command => sub { exit })->pack;
+$mw->Button(-text => 'Andar no sistema', -command => sub { exit })->pack;
+$mw->Button(-text => 'Acompanhar Mes', -command => sub { exit })->pack;
+$mw->Button(-text => 'Média contas', -command => sub { exit })->pack;
+
+
+$mw->Button(-text => "Exit",
+            -command => sub { exit })->pack(-side => 'bottom', 
+                                            -expand => 1,
+                                            -fill => 'x');
+
+#$mw->Button(-text => "TESTE", -command => sub { exit })->pack;
 MainLoop;
